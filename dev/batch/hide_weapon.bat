fa7779862abd7ce79aaaf2342512aa8a4ca51802
a-horsey <109042532+a-horsey@users.noreply.github.com>
2022-07-20 22:11:19 +0200

    improved hiding, fixed bugs

--- dev/batch/hide_weapon.bat
+++ dev/batch/hide_weapon.bat
@@ -1,14 +1,14 @@
 @echo off
 
 ::set values
-set hide_origin_value=origin 0 -500 0
+set hide_origin_value=origin 0 -999 0
 set hide_origin_value_keep_flames_visible=origin 0 -85 35
 set hide_origin_value_keep_beam_visible= origin 0 -85 55
 
 ::exit if hidden is no
 IF NOT %•Hidden(y/n)%==y goto :EOF
 ::apply values for beam/flames
-IF %qc_file%==c_medic_animations.qc set hide_origin_value=origin 9999 9999 9999
+IF %attack_sequence_2%==fire_loop set hide_origin_value=origin 9999 9999 9999
 IF %Keep_flames_visible(y/n)%==y set hide_origin_value=%hide_origin_value_keep_flames_visible%
 IF %Keep_beam_visible(y/n)%==y set hide_origin_value=%hide_origin_value_keep_beam_visible%
 cd "%qc_folder_temp%"
