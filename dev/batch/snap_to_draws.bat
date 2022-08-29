e178fd328d5437832cd443f41942ee1fbc76ac5d
a-horsey <109042532+a-horsey@users.noreply.github.com>
2022-08-29 12:19:56 +0200

    improved draws transitions

--- dev/batch/snap_to_draws.bat
+++ dev/batch/snap_to_draws.bat
@@ -1,4 +1,6 @@
-::adds snap (fadein 0.0) to all draws to avoid blending issues
+::adds snap and fade values to all draws to avoid blending issues
+::both of them aren't technically needed but fadein and fadeout were sometimes not enough
+::added snap just in case
 IF %draw_sequence%==none goto :EOF
 cd "%qc_folder_temp%"
-echo $append %draw_sequence% fadein 0.0 fadeout 0.1 >> %qc_file%
+echo $append %draw_sequence% snap fadein 0.0 fadeout 0.1 >> %qc_file%
