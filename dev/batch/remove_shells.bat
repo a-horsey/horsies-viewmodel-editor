16af8eb3946b39d67092de0a0ecd31217ae56505
a-horsey <109042532+a-horsey@users.noreply.github.com>
2022-10-09 23:45:33 +0200

    Delete dev directory

--- dev/batch/remove_shells.bat
+++
@@ -1,4 +0,0 @@
-IF NOT %•Remove_shells(y/n)%==y goto :EOF
-cd "%qc_folder_temp%"
-findstr /i /v /l "6002" %qc_file% >> remove_shells_temp
-move remove_shells_temp %qc_file% >nul
--- dev/preload_files/readme.txt
+++
@@ -1,3 +0,0 @@
-Made using Horsie's Viewmodel Editor
-https://github.com/a-horsey/horsies-viewmodel-editor
-https://gamebanana.com/tools/10146
